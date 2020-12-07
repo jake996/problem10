9e436bb146937af46e628ef39a5b67308ac59109
Jake <jacoba1@kpit.com>
2020-12-07 13:36:09 +0530

    fixing range of diode voltage

--- diode.m
+++ diode.m
@@ -2,7 +2,7 @@
 i0=2e-4; % leakage current
 k=1.38e-23;% boltzman constant
 q=1.602e-19;% charge of electron in C
-Vd=-0.2:0.01:.9; % diode voltage
+Vd=-0.2:0.01:1; % diode voltage
 tempinF=[75 200 400];
 for ii=1:length(tempinF)
     tempinF=(5/9)*(tempinF(ii)-32)+273.15; %converting
