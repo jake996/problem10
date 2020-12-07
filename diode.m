b055661adac9d377dbb780c855f5e4e2a1acf92b
Jake <jacoba1@kpit.com>
2020-12-07 13:35:22 +0530

    script created

--- diode.m
+++ diode.m
@@ -2,3 +2,10 @@
 i0=2e-4; % leakage current
 k=1.38e-23;% boltzman constant
 q=1.602e-19;% charge of electron in C
+Vd=-0.2:0.01:.9; % diode voltage
+tempinF=[75 200 400];
+for ii=1:length(tempinF)
+    tempinF=(5/9)*(tempinF(ii)-32)+273.15; %converting
+    id=i0.*(exp((q*Vd)/(k*tempinF))-1)
+    plot(Vd,id,'-ok');
+end
