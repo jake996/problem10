fa72bbaa5b85342715b90a23568f3a76c4526351
Jake <jacoba1@kpit.com>
2020-12-07 15:38:16 +0530

    dummy input script created

--- script/diode.m
+++ script/diode.m
@@ -4,10 +4,6 @@ k=1.38e-23;% boltzman constant
 q=1.602e-19;% charge of electron in C
 Vd=-0.2:0.01:1; % diode voltage
 tempinF=[75 200 400];
-Vd=Vd';
-t=0:0.01:1.2;
-t=t';
-
 for ii=1:length(tempinF)
     tempinF=(5/9)*(tempinF(ii)-32)+273.15; %converting
     id=i0.*(exp((q*Vd)/(k*tempinF))-1)
---
+++ script/dummyinput.m
@@ -0,0 +1,4 @@
+t=0:0.01:10;
+t=t';
+volt=sin(t)
+sim('problem10.slx');
